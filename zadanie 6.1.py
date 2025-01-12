e2031eae12d52df4299ccb868a320ec040465ab7
levandovsky11 <mod2004bl@gmail.com>
2025-01-12 10:17:34 +0100

    Druga (poprawiona) wersja zadania

--- zadanie 6.1.py
+++ zadanie 6.1.py
@@ -1,4 +1,21 @@
 import os
 
+def wyswietl(folder, poziom=0):
+    try:
+        elementy = os.listdir(folder)
+    except PermissionError:
+        print(" " * (poziom * 2) + "⛔ brak dostępu ⛔")
+        return
+
+    for element in elementy:
+        sciezka = os.path.join(folder, element)
+        if os.path.isdir(sciezka):
+            print(" " * (poziom * 2) + f"📁{element}")
+            wyswietl(sciezka, poziom + 2)
+        else:
+            print(" " * (poziom * 2) + f"📃{element}")
+
+sciezka = input("podaj ścieżkę do folderu: ")
+wyswietl(sciezka)
 
 
